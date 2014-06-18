1711a0406f9dc2900428be54ca0c7b53c1b46ec0
Kyle Kneitinger <kylejkneitinger@gmail.com>
2014-06-18 09:29:24 +0000

    Added block to demonstrate pin toggling

--- examples/RPiShift_demo.py
+++ examples/RPiShift_demo.py
@@ -6,19 +6,17 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(23, GPIO.IN)
 
-Shiftr =RPiShift.Shiftr(15,11,13)
+Shiftr = RPiShift.Shiftr(15,11,13)
 # Make sure everything is hooked up
 def testFunc():
-    # Test bit pushing
+    # Test byte writing by counting up and down in binary
     for i in range(256**Shiftr.CHAIN):
-        Shiftr.pushBit(i)
-        Shiftr.writeLatch()
+        Shiftr.writeByte(i)
         sleep(.008)
     for i in range((256**Shiftr.CHAIN)-1,0,-1):
-        Shiftr.pushBit(i)
-        Shiftr.writeLatch()
+        Shiftr.writeByte(i)
         sleep(.008)
-        
+
     # Test individual pin writing
     for i in range(4*Shiftr.CHAIN):
         Shiftr.writePin(i,1)
@@ -28,12 +26,13 @@ def testFunc():
         sleep(.25)
         Shiftr.writePin(i+4,0)
 
-    # Test byte writing
-    Shiftr.writeByte(0xFF)
-    sleep(.25)
-    Shiftr.writeByte(0x0F)
-    sleep(.25)
-    Shiftr.writeByte(0xF0)
-    sleep(.25)
-    Shiftr.cleanup()
+    # Turn on every other pin
+    Shiftr.writeByte(0x55)
+    # Toggle between 0b01010101 and 0b10101010
+    for i in range(8):
+        for j in range(8):
+            Shiftr.togglePin(j)
+        sleep(.125)
+
 testFunc()
+Shiftr.cleanup()
